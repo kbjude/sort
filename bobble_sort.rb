ed83b9dfbb632635f527effd30b64f2e7f572ff1
Meme <meme_es@hotmail.com>
2020-03-25 09:24:56 -0600

    Fix linters
    This commit fix linters in the boble_sort method

--- bobble_sort.rb
+++ bobble_sort.rb
@@ -1,16 +1,18 @@
 def bobble_sort(array)
-  counter = array.length
-  begin
+  counter = array.length - 1
+  loop do
     new_counter = 0
-    for i in (1...counter)
+    counter.times do |i|
       if array[i - 1] > array[i]
         array[i - 1], array[i] = array[i], array[i - 1]
         new_counter = i
       end
-    end    
+    end
+
     counter = new_counter
-  end until counter <= 1
-  return array
+    break if counter <= 1
+  end
+  array
 end
 
 print bobble_sort([4, 9, 6, 5, 3, 10, 11, 1, 2])
